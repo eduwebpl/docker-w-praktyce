5a4f93fc78d8aa041ca71255164541884a84b905
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2022-03-10 14:02:27 +0100

    04-finish

--- 04/server.js
+++ 04/server.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs').promises;
 
-const requestListener = function (req, res) {
+const requestListener = async function (req, res) {
+  const dataToServe = (await fs.readFile('/index.html')).toString('utf-8');
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end(dataToServe);
 }
 
 const server = http.createServer(requestListener);
