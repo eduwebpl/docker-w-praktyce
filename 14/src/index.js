8725317a8fa0ab8d83d42d233e284588712b4f82
Wojciech Połowniak <wojciech.polowniak@gmail.com>
2022-03-11 10:50:07 +0100

    14

---
+++ 14/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.static('src/static'));
+ 
+app.get('/hello', (req, res) => {
+ res.send('Hello world from my API!');
+});
+
+app.listen(port);
+// setInterval(() => {
+ 
+//  console.log('hello world');
+// }, 1000);
